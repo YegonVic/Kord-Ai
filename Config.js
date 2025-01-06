aa9a00ceb644cc6a40405fd0c0e313617c1bf32c
YegonVic <161466556+YegonVic@users.noreply.github.com>
2025-01-06 18:44:47 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
+  OWNER_NUMBERS: "254758196672", // your number with country code
+  OWNER_NAME: 'Kalvix', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: '', // put your session id between the ''
+  SESSION_ID: 'kord_ai-E04dxVhTxbsp', // put your session id between the ''
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
